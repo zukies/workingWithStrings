a66a21ebc97974801c6a1b7c69add2fedf238cdc
zukies <zukies28@gmail.com>
2022-03-09 14:56:47 +0200

    Update workingWithStrings.js

---
+++ workingWithStrings.js
@@ -0,0 +1,31 @@
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+
+  const splitText = text.split("\n");
+
+  const arr = [];
+  for (const rows of splitText) {
+    const indexof = rows.indexOf("_") + 1;
+
+    const nextLetterUpper = rows.replace(
+      rows.slice(indexof - 1, indexof + 1),
+      rows.slice(indexof - 1, indexof + 1).toUpperCase()
+    );
+    const slicedFromUpperLetter = nextLetterUpper.slice(indexof);
+
+    const lowerCaseLetters = rows
+      .slice(0, indexof - 1)
+      .toLowerCase()
+      .trim();
+
+    emoji = "⌛️";
+    repeatEmoji = emoji.repeat(arr.length + 1);
+
+    arr.push(lowerCaseLetters + slicedFromUpperLetter + repeatEmoji);
+  }
+
+  console.log(arr.join("\n"));
+  // const rows = text.split('\n');
+});
