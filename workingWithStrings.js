cba948fcf3944c612609bffd591f3d4a3e408761
zukies <zukies28@gmail.com>
2022-03-10 08:37:07 +0200

    Update workingWithStrings.js
    Changes made to js file

--- workingWithStrings.js
+++ workingWithStrings.js
@@ -2,30 +2,30 @@ document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
 document.querySelector("button").addEventListener("click", function () {
   const text = document.querySelector("textarea").value;
-
   const splitText = text.split("\n");
+  // const arr = [];
+  for (const data of splitText.entries()) {
+    const [i, rows] = data;
 
-  const arr = [];
-  for (const rows of splitText) {
     const indexof = rows.indexOf("_") + 1;
 
     const nextLetterUpper = rows.replace(
       rows.slice(indexof - 1, indexof + 1),
       rows.slice(indexof - 1, indexof + 1).toUpperCase()
     );
-    const slicedFromUpperLetter = nextLetterUpper.slice(indexof);
 
+    const slicedFromUpperLetter = nextLetterUpper.slice(indexof);
     const lowerCaseLetters = rows
       .slice(0, indexof - 1)
       .toLowerCase()
       .trim();
 
-    emoji = "⌛️";
-    repeatEmoji = emoji.repeat(arr.length + 1);
+    repeatEmoji = "⌛️".repeat(i + 1);
 
-    arr.push(lowerCaseLetters + slicedFromUpperLetter + repeatEmoji);
+    const all = (lowerCaseLetters + slicedFromUpperLetter).padEnd(20);
+
+    console.log(`${all}${repeatEmoji}`);
   }
-
-  console.log(arr.join("\n"));
+  // console.log(arr.join("\n"));
   // const rows = text.split('\n');
 });
